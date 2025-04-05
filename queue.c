e2b4971e5e1cc9f8df26ca9b20695b47d7ace0c0
23liua <156217746+23liua@users.noreply.github.com>
2025-04-04 22:42:49 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -5,47 +5,55 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+void enqueue(struct queue *qq, struct game_state st) {
+    uint64_t coded = serialize(st);
+    insert_at_tail(&(qq->data), (size_t)coded);
+}
+
+struct game_state dequeue(struct queue *qq) {
+    size_t sVal = remove_from_head(&(qq->data));
+    return deserialize((uint64_t)sVal);
+}
+
 #define VISITSIZE 88801
-#define MAX_NEIGHBORS 4
 
 typedef struct visit_node {
-    uint64_t state_hash;
-    struct visit_node *next;
+    uint64_t rep;
+    struct visit_node *chain;
 } visit_node;
 
-static visit_node *visited[VISITSIZE];
+static visit_node *visitTable[VISITSIZE];
 
-static unsigned hash_state(uint64_t key) {
+static unsigned get_index(uint64_t key) {
     return (unsigned)(key % VISITSIZE);
 }
 
-static bool state_visited(uint64_t key) {
-    visit_node *node = visited[hash_state(key)];
-    while (node) {
-        if (node->state_hash == key) return true;
-        node = node->next;
+static bool visited_has(uint64_t key) {
+    unsigned i = get_index(key);
+    for (visit_node *cursor = visitTable[i]; cursor; cursor = cursor->chain) {
+        if (cursor->rep == key) return true;
     }
     return false;
 }
 
-static void mark_visited(uint64_t key) {
-    unsigned index = hash_state(key);
-    visit_node *new_node = malloc(sizeof(*new_node));
-    if (!new_node) exit(2);
-    new_node->state_hash = key;
-    new_node->next = visited[index];
-    visited[index] = new_node;
+static void visited_add(uint64_t key) {
+    unsigned i = get_index(key);
+    visit_node *tmp = malloc(sizeof(visit_node));
+    if (!tmp) exit(2);
+    tmp->rep   = key;
+    tmp->chain = visitTable[i];
+    visitTable[i] = tmp;
 }
 
-static void clear_visited() {
-    for (int i = 0; i < VISITSIZE; i++) {
-        visit_node *node = visited[i];
-        while (node) {
-            visit_node *temp = node;
-            node = node->next;
-            free(temp);
+static void visited_cleanup() {
+    for (int j = 0; j < VISITSIZE; j++) {
+        visit_node *ptr = visitTable[j];
+        while (ptr) {
+            visit_node *gone = ptr;
+            ptr = ptr->chain;
+            free(gone);
         }
-        visited[i] = NULL;
+        visitTable[j] = NULL;
     }
 }
 
@@ -55,83 +63,75 @@ static bool is_solved(struct game_state s) {
         for (int c = 0; c < 4; c++) {
             if (r == 3 && c == 3) {
                 if (s.tiles[r][c] != 0) return false;
-            } else if (s.tiles[r][c] != need++) {
-                return false;
+            } else {
+                if (s.tiles[r][c] != need) return false;
+                need++;
             }
         }
     }
     return true;
 }
 
-static int generate_successors(struct game_state state, struct game_state neighbors[]) {
-    struct game_state temp;
-    int count = 0;
-    
-    temp = state;
-    if (move_left(&temp)) {
-        neighbors[count++] = temp;
-    }
-    
-    temp = state;
-    if (move_right(&temp)) {
-        neighbors[count++] = temp;
-    }
-    
-    temp = state;
-    if (move_up(&temp)) {
-        neighbors[count++] = temp;
-    }
-    
-    temp = state;
-    if (move_down(&temp)) {
-        neighbors[count++] = temp;
-    }
-    
-    return count;
-}
-
-void enqueue(struct queue *q, struct game_state state) {
-    uint64_t val = serialize(state);
-    insert_at_tail(&q->data, val);
-}
-
-struct game_state dequeue(struct queue *q) {
-    uint64_t val = remove_from_head(&q->data);
-    return deserialize(val);
+static void flush_queue(struct queue *qq) {
+    free_list(qq->data);
+    qq->data.head = NULL;
 }
 
 int number_of_moves(struct game_state start) {
-    if (is_solved(start)) return start.num_steps;
-    
-    // Initialize visited set
-    for (int i = 0; i < VISITSIZE; i++) visited[i] = NULL;
-    
-    struct queue q = {0};
-    mark_visited(serialize(start));
-    enqueue(&q, start);
+    if (is_solved(start)) {
+        return start.num_steps;
+    }
+    for (int c = 0; c < VISITSIZE; c++) {
+        visitTable[c] = NULL;
+    }
+    struct queue store;
+    store.data.head = NULL;
+    uint64_t origin = serialize(start);
+    visited_add(origin);
+    enqueue(&store, start);
+    while (store.data.head) {
+        struct game_state top = dequeue(&store);
+        if (is_solved(top)) {
+            visited_cleanup();
+            flush_queue(&store);
+            return top.num_steps;
+        }
+        uint64_t top_id = serialize(top);
+        struct game_state attempt;
+        uint64_t next_id;
 
-    while (q.data.head) {
-        struct game_state current = dequeue(&q);
-        
-        if (is_solved(current)) {
-            int moves = current.num_steps;
-            clear_visited();
-            free_list(q.data);
-            return moves;
+        attempt = top;
+        move_down(&attempt);
+        next_id = serialize(attempt);
+        if (next_id != top_id && !visited_has(next_id)) {
+            visited_add(next_id);
+            enqueue(&store, attempt);
         }
 
-        struct game_state neighbors[MAX_NEIGHBORS];
-        int count = generate_successors(current, neighbors);
+        attempt = top;
+        move_right(&attempt);
+        next_id = serialize(attempt);
+        if (next_id != top_id && !visited_has(next_id)) {
+            visited_add(next_id);
+            enqueue(&store, attempt);
+        }
 
-        for (int i = 0; i < count; i++) {
-            uint64_t neighbor_hash = serialize(neighbors[i]);
-            if (!state_visited(neighbor_hash)) {
-                mark_visited(neighbor_hash);
-                enqueue(&q, neighbors[i]);
-            }
+        attempt = top;
+        move_up(&attempt);
+        next_id = serialize(attempt);
+        if (next_id != top_id && !visited_has(next_id)) {
+            visited_add(next_id);
+            enqueue(&store, attempt);
+        }
+
+        attempt = top;
+        move_left(&attempt);
+        next_id = serialize(attempt);
+        if (next_id != top_id && !visited_has(next_id)) {
+            visited_add(next_id);
+            enqueue(&store, attempt);
         }
     }
-
-    clear_visited();
+    visited_cleanup();
     return -1;
 }
