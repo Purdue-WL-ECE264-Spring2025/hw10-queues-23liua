85077452cb1990e57e1d41b49c9df263ab1a197c
23liua <156217746+23liua@users.noreply.github.com>
2025-04-04 21:27:39 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,149 +1,46 @@
 #include "queue.h"
-#include "linked_list.h"
 #include "tile_game.h"
-
-#include <stdbool.h>
 #include <stdlib.h>
 
-// helper that checks if a serialized state is in a linked list
-static bool in_list(struct linked_list *lst, size_t val) {
-    for (struct list_node *cur = lst->head; cur != NULL; cur = cur->next) {
-        if (cur->value == val) {
-            return true;
-        }
-    }
-    return false;
-}
-
-// enqueue: serialize and insert at tail
+// adds game state to back of queue
 void enqueue(struct queue *q, struct game_state state) {
-    size_t encoded = serialize(state);
-    insert_at_tail(&q->list, encoded);
+    size_t serialized = serialize(state);
+    insert_at_tail(&q->list, serialized);
 }
 
-// dequeue: remove from head and deserialize
+// removes and returns game state from front of queue
 struct game_state dequeue(struct queue *q) {
-    size_t encoded = remove_from_head(&q->list);
-    return deserialize(encoded);
+    size_t serialized = remove_from_head(&q->list);
+    return deserialize(serialized);
 }
 
-// returns how many moves are needed to reach the solved state, or -1 if not found
+// finds shortest path to solve the tiles game
 int number_of_moves(struct game_state start) {
-    // define the solved state
-    struct game_state solved;
-    int tile = 1;
-    for (int r = 0; r < 4; r++) {
-        for (int c = 0; c < 4; c++) {
-            solved.board[r][c] = tile++;
-        }
-    }
-    // last spot is 0
-    solved.board[3][3] = 0;
-    solved.empty_r = 3;
-    solved.empty_c = 3;
-    solved.moves = 0;
-
-    size_t solved_int = serialize(solved);
-    size_t start_int  = serialize(start);
-
-    // check if we are already solved
-    if (start_int == solved_int) {
+    // already solved
+    if (is_solved(start)) {
         return 0;
     }
 
-    // set up queue
-    struct queue q;
-    q.list.head = NULL;
+    struct queue q = {0};
     enqueue(&q, start);
 
-    // track visited states
-    struct linked_list visited;
-    visited.head = NULL;
-    insert_at_tail(&visited, start_int);
-
-    // bfs
+    // bfs to explore all possible moves
     while (q.list.head != NULL) {
         struct game_state current = dequeue(&q);
-        size_t current_int = serialize(current);
 
-        // if solved, free and return
-        if (current_int == solved_int) {
-            free_list(q.list);      // free nodes in queue
-            free_list(visited);     // free nodes in visited
-            return current.moves;
-        }
-
-        int r = current.empty_r;
-        int c = current.empty_c;
-
-        // 4 possible slides: from above, below, left, right
-
-        // slide tile in from above
-        if (r > 0) {
-            struct game_state next = current;
-            next.board[r][c] = next.board[r - 1][c];
-            next.board[r - 1][c] = 0;
-            next.empty_r = r - 1;
-            next.empty_c = c;
-            next.moves = current.moves + 1;
-
-            size_t next_int = serialize(next);
-            if (!in_list(&visited, next_int)) {
-                insert_at_tail(&visited, next_int);
-                enqueue(&q, next);
-            }
-        }
-
-        // slide tile in from below
-        if (r < 3) {
-            struct game_state next = current;
-            next.board[r][c] = next.board[r + 1][c];
-            next.board[r + 1][c] = 0;
-            next.empty_r = r + 1;
-            next.empty_c = c;
-            next.moves = current.moves + 1;
-
-            size_t next_int = serialize(next);
-            if (!in_list(&visited, next_int)) {
-                insert_at_tail(&visited, next_int);
-                enqueue(&q, next);
-            }
-        }
-
-        // slide tile in from the left
-        if (c > 0) {
-            struct game_state next = current;
-            next.board[r][c] = next.board[r][c - 1];
-            next.board[r][c - 1] = 0;
-            next.empty_r = r;
-            next.empty_c = c - 1;
-            next.moves = current.moves + 1;
-
-            size_t next_int = serialize(next);
-            if (!in_list(&visited, next_int)) {
-                insert_at_tail(&visited, next_int);
-                enqueue(&q, next);
-            }
-        }
-
-        // slide tile in from the right
-        if (c < 3) {
-            struct game_state next = current;
-            next.board[r][c] = next.board[r][c + 1];
-            next.board[r][c + 1] = 0;
-            next.empty_r = r;
-            next.empty_c = c + 1;
-            next.moves = current.moves + 1;
-
-            size_t next_int = serialize(next);
-            if (!in_list(&visited, next_int)) {
-                insert_at_tail(&visited, next_int);
+        // try sliding in all 4 directions
+        for (int direction = 0; direction < 4; direction++) {
+            if (can_slide(current, direction)) {
+                struct game_state next = slide(current, direction);
+                if (is_solved(next)) {
+                    free_list(q.list);
+                    return next.number_of_moves;
+                }
                 enqueue(&q, next);
             }
         }
     }
 
-    // if we exhaust the queue without finding a solution
-    free_list(visited);
-    return -1;
+    free_list(q.list);
+    return -1; // shouldnt happen if puzzle is solvable
 }
