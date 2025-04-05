9ee508822b1a8d9a2e98ea6e4aa3aa8fa64fc4f6
23liua <156217746+23liua@users.noreply.github.com>
2025-04-04 22:31:25 -0400

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -1,87 +1,97 @@
 #include "linked_list.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <stdio.h> 
 
-struct list_node *create_node(size_t val) {
-    struct list_node *node = malloc(sizeof(struct list_node));
+
+typedef struct custom_node {
+    size_t value;
+    struct custom_node *next;
+    unsigned char metadata; 
+} custom_node_t;
+
+struct linked_list {
+    custom_node_t *head;
+};
+
+struct list_node *new_node(size_t value) {
+    custom_node_t *node = (custom_node_t *)malloc(sizeof(custom_node_t));
     if (node) {
-        node->value = val;
+        node->value = value;
         node->next = NULL;
+        node->metadata = 0;
     }
-    return node;
+    return (struct list_node *)node;
 }
 
-void insert_at_head(struct linked_list *lst, size_t num) {
-    struct list_node *new = create_node(num);
-    if (!new) return;
-    
-    new->next = lst->head;
-    lst->head = new;
+void insert_at_head(struct linked_list *list, size_t value) {
+    if (!list) return;
+    custom_node_t *node = (custom_node_t *)new_node(value);
+    if (node) {
+        node->next = list->head;
+        list->head = node;
+    }
 }
 
-void append_at_tail(struct linked_list *lst, size_t val) {
-    struct list_node *node = create_node(val);
-    if (!node) return;
-
-    if (!lst->head) {
-        lst->head = node;
-        return;
+void insert_at_tail(struct linked_list *list, size_t value) {
+    if (!list) return;
+    custom_node_t *node = (custom_node_t *)new_node(value);
+    if (node) {
+        if (!list->head) {
+            list->head = node;
+        } else {
+            custom_node_t *current = list->head;
+            while (current->next) {
+                current = current->next;
+            }
+            current->next = node;
+        }
     }
-
-    struct list_node *current = lst->head;
-    while (current->next) {
-        current = current->next;
-    }
-    current->next = node;
 }
 
-size_t pop_from_head(struct linked_list *lst) {
-    if (!lst->head) return 0;
-    
-    struct list_node *temp = lst->head;
+size_t remove_from_head(struct linked_list *list) {
+    if (!list || !list->head) return 0;
+    custom_node_t *temp = list->head;
     size_t value = temp->value;
-    lst->head = temp->next;
+    list->head = temp->next;
     free(temp);
-    
     return value;
 }
 
-size_t remove_last(struct linked_list *lst) {
-    if (!lst->head) return 0;
-
-    struct list_node *current = lst->head;
-    struct list_node *previous = NULL;
-
+size_t remove_from_tail(struct linked_list *list) {
+    if (!list || !list->head) return 0;
+    if (!list->head->next) {
+        size_t value = list->head->value;
+        free(list->head);
+        list->head = NULL;
+        return value;
+    }
+    custom_node_t *current = list->head;
+    custom_node_t *previous = NULL;
     while (current->next) {
         previous = current;
         current = current->next;
     }
-
-    size_t val = current->value;
-    if (!previous) {
-        lst->head = NULL;
-    } else {
-        previous->next = NULL;
-    }
+    size_t value = current->value;
+    previous->next = NULL;
     free(current);
-    
-    return val;
+    return value;
 }
 
-void destroy_list(struct linked_list *list) {
-    struct list_node *current = list->head;
+void free_list(struct linked_list list) {
+    custom_node_t *current = list.head;
     while (current) {
-        struct list_node *next = current->next;
+        custom_node_t *next = current->next;
         free(current);
         current = next;
     }
-    list->head = NULL;
+    // Note: We are passing the list by value, so we cannot directly set list.head to NULL here.
 }
 
-void display_list(FILE *fp, struct linked_list *list) {
-    fprintf(fp, "{ ");
-    for (struct list_node *node = list->head; node; node = node->next) {
-        fprintf(fp, "%zu ", node->value);
+// Utility function to help you debugging, do not modify
+void dump_list(FILE *fp, struct linked_list list) {
+    fprintf(fp, "[ ");
+    for (struct list_node *cur = (struct list_node *)list.head; cur != NULL; cur = cur->next) {
+        fprintf(fp, "%zu ", cur->value);
     }
-    fprintf(fp, "}\n");
+    fprintf(fp, "]\n");
 }
